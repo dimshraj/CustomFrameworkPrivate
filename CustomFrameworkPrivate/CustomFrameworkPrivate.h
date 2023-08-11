b96b23d1902f44465fb8d790b26a81f28395a312
shraiber <shraiber@ujin.tech>
2023-08-12 03:19:53 +0500

    Initial Commit

---
+++ CustomFrameworkPrivate/CustomFrameworkPrivate.h
@@ -0,0 +1,18 @@
+//
+//  CustomFrameworkPrivate.h
+//  CustomFrameworkPrivate
+//
+//  Created by Dmitriy Shrayber on 12.08.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomFrameworkPrivate.
+FOUNDATION_EXPORT double CustomFrameworkPrivateVersionNumber;
+
+//! Project version string for CustomFrameworkPrivate.
+FOUNDATION_EXPORT const unsigned char CustomFrameworkPrivateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomFrameworkPrivate/PublicHeader.h>
+
+
